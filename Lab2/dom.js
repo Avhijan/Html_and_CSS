eca2a10de18d8e28382f29e3b42395ce6ac44d56
Avhijan <abhijansap06@gmail.com>
2025-12-22 18:55:19 +0545

    JS for html elements
    Using JS to create and change html elements or add style propertites.

---
+++ Lab2/dom.js
@@ -0,0 +1,38 @@
+// console.log(document)
+// document.title= "DOM";
+// document.body.style.backgroundColor ='red';
+// document.getElementById('header').style.color='white';
+// document.getElementById('header').textContent="AbC";
+
+// document.getElementsByClassName('anchor')[0].style.color='white';
+
+
+// document.querySelector('#header').style.color="blue"
+// document.querySelector('.anchor').style.color='white';
+
+// console.log(document.querySelectorAll('.anchor'))
+
+// document.querySelector('#abc').textContent ="hallo guest";
+// document.querySelector('.xyz').innerHTML= '<a href = "#"> LInk </a>';
+
+// isToggle= false;
+// function sayHi()
+// {
+//     if(isToggle){
+//         document.querySelector('#header').textContent="Hallo";
+//     }else
+// {
+//         document.querySelector('#header').textContent="Hi!";
+//     }
+//     isToggle=!isToggle;
+
+// }
+
+newButton = document.createElement('button');
+newButton.innerText = 'CLick';
+document.querySelector('body').appendChild(newButton)
+
+newlink = document.createElement('a');
+newlink.innerText='naya link';
+newlink.href='#'
+document.querySelector('body').appendChild(newlink);
